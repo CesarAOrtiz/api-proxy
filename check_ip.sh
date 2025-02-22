be86794d7113d6fa6a2ad96d2d2d979a26f53269
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2025-02-22 04:19:01 +0000

    sin rotate_ip.sh

--- check_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "🔍 Iniciando validación de IPs de Tor..."
-while true; do
-    echo "🌐 Nueva IP de Tor:"
-    curl --socks5-hostname 127.0.0.1:9050 -s https://check.torproject.org/api/ip
-    sleep 30
-done
