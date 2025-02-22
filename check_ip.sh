ecbeb52f64cdee350bd29027cefec518a29b5a16
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2025-02-22 04:36:32 +0000

    check_ip eliminado

--- check_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "🔍 Iniciando validación de IPs de Tor..."
-while true; do
-    IP=$(curl --socks5-hostname 127.0.0.1:9050 -s https://check.torproject.org/api/ip)
-    echo "🌐 Nueva IP de Tor: $IP"
-    sleep 10
-done
